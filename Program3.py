999d6fcf56615e039e330fabe866abbae842a85e
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-03-23 21:47:13 +0800

    Format

--- Program3.py
+++ Program3.py
@@ -1,5 +1,9 @@
 #Pseudocode
 
+import pyfiglet
+
+print()
+
 #Prompt user to input name
 name = input("What is your name? ")
 
@@ -13,12 +17,20 @@ favorite_food = input("What is your favorite food? ")
 favorite_color = input("What is your favorite color? ")
 
 #Format inputted name in a fancy way
+name_art1 = pyfiglet.figlet_format("Hello!", font="cybermedium")
+name_art2 = pyfiglet.figlet_format(name, font="univers")
 
 #Format inputted dream job in a fancy way
+dream_job_art1 = pyfiglet.figlet_format("Your Dream Job is: ", font = "cybermedium")
+dream_job_art2 = pyfiglet.figlet_format(dream_job, foont = "univers")
 
 #Format inputted favorite food in a fancy way
+favorite_food_art1 = pyfiglet.figlet_format("Your Favorite Food is: ", font = "cybermedium")
+favorite_food_art2 = pyfiglet.figlet_format(favorite_food, font = "univers")
 
 #Format inputted favorite color in a fancy way
+favorite_color_art1 = pyfiglet.figlet_format("Your Favorite Color is: ", font = "cybermedium")
+favorite_color_art2 = pyfiglet.figlet_format(favorite_color, font = "univers")
 
 #Display formatted name in a fancy way and in other color
 
