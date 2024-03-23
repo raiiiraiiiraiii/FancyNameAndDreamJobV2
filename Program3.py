66273efe7ab0b90629c4febcef8df85e7f2750b7
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-03-23 21:51:12 +0800

    Display and Color

--- Program3.py
+++ Program3.py
@@ -22,7 +22,7 @@ name_art2 = pyfiglet.figlet_format(name, font="univers")
 
 #Format inputted dream job in a fancy way
 dream_job_art1 = pyfiglet.figlet_format("Your Dream Job is: ", font = "cybermedium")
-dream_job_art2 = pyfiglet.figlet_format(dream_job, foont = "univers")
+dream_job_art2 = pyfiglet.figlet_format(dream_job, font = "univers")
 
 #Format inputted favorite food in a fancy way
 favorite_food_art1 = pyfiglet.figlet_format("Your Favorite Food is: ", font = "cybermedium")
@@ -33,9 +33,13 @@ favorite_color_art1 = pyfiglet.figlet_format("Your Favorite Color is: ", font = 
 favorite_color_art2 = pyfiglet.figlet_format(favorite_color, font = "univers")
 
 #Display formatted name in a fancy way and in other color
+print("\033[93m{}\033[00m".format(name_art1 + name_art2))
 
 #Display formatted dream job in a fancy way and in other color
+print("\033[93m{}\033[00m".format(dream_job_art1 + dream_job_art2))
 
 #Display formatted favorite food in a fancy way and in other color
+print("\033[93m{}\033[00m".format(favorite_food_art1 + favorite_food_art2))
 
 #Display formatted favorite color in a fancy way and in other color
+print("\033[93m{}\033[00m".format(favorite_color_art1 + favorite_color_art2))
