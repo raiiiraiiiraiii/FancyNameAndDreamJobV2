905a31d986331ea7413e3c87c2da0adba1b06c09
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-03-23 21:37:28 +0800

    Prompt User

--- Program3.py
+++ Program3.py
@@ -1,12 +1,16 @@
 #Pseudocode
 
 #Prompt user to input name
+name = input("What is your name? ")
 
 #Prompt user to input dream job
+dream_job = input("What is your dream job? ")
 
 #Prompt user to input favorite food
+favorite_food = input("What is your favorite food? ")
 
 #Prompt user to input favorite color
+favorite_color = input("What is your favorite color? ")
 
 #Format inputted name in a fancy way
 
