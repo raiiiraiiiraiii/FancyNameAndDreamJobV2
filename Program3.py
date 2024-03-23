38f29cef009a0764fabb3d7a1ec6c80b671a5bc8
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-03-23 19:44:19 +0800

    Add Pseudocode

--- Program3.py
+++ Program3.py
@@ -1,1 +1,10 @@
-#NAUUURRR
+#Pseudocode
+
+#Prompt user to input name
+#Prompt user to input dream job
+#Prompt user to input favorite food
+#Prompt user to input favorite color
+#Display inputted name in a fancy way
+#Display inputted dream job in a fancy way
+#Display inputted favorite food in a fancy way
+#Display inputted favorite color in a fancy way
