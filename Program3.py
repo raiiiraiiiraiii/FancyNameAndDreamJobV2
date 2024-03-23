90d5b59504d2e7a3022024a589aabe4d3e4ab256
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-03-23 21:54:45 +0800

    Additional Touch

--- Program3.py
+++ Program3.py
@@ -3,7 +3,7 @@
 import pyfiglet
 
 print()
-
+print("\033[93m==================================================================================\033[00m")
 #Prompt user to input name
 name = input("What is your name? ")
 
@@ -15,6 +15,8 @@ favorite_food = input("What is your favorite food? ")
 
 #Prompt user to input favorite color
 favorite_color = input("What is your favorite color? ")
+
+print()
 
 #Format inputted name in a fancy way
 name_art1 = pyfiglet.figlet_format("Hello!", font="cybermedium")
@@ -43,3 +45,4 @@ print("\033[93m{}\033[00m".format(favorite_food_art1 + favorite_food_art2))
 
 #Display formatted favorite color in a fancy way and in other color
 print("\033[93m{}\033[00m".format(favorite_color_art1 + favorite_color_art2))
+print("\033[93m==================================================================================\033[00m")
